bc376c138dc13e2545751d1db39b7c1d89b0b53d
Ben1055 <124049378+Ben1055@users.noreply.github.com>
2023-01-31 17:20:43 +0530

    First Commit of Entire Source
    original source

---
+++ TEST/src/com/ben/inter/Main.java
@@ -0,0 +1,47 @@
+package com.ben.inter;
+
+import  java.util.*;
+
+public class Main
+{
+    
+	static String minWindow(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        
+        int tIdx = 0 ;
+        int minWindow = (int)Math.pow(10,9);
+        
+        int startIdx = -1;
+        
+        int left = 0;
+        int right = 0;
+        
+        while(right < n){
+            if(str1.charAt(right) == str2.charAt(tIdx)){
+            	System.out.println("right "+right);
+                if(tIdx == m-1){
+                    while(left<=right && str1.charAt(left) != str2.charAt(0)){
+                        left++;
+                    }
+                    if(right-left+1 < minWindow){
+                        minWindow = right-left + 1;
+                        startIdx = left;
+                    }
+                 
+                    right = left;
+                    left ++;
+                    
+                    tIdx = 0;
+                }
+                else
+                	tIdx++;
+            }
+            right ++;
+        }
+        return str1.substring(startIdx, startIdx+minWindow);
+    }
+	public static void main(String[] args) {
+		System.out.println(minWindow("geeksforgeeks","eksrg"));
+	}
+}
